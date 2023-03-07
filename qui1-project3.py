b229b6b589ead5240575c2da848ebfe887d93eac
Shiven Desai <shiven.desai14@gmail.com>
2023-03-06 19:08:22 -0800

    Made project 3

---
+++ qui1-project3.py
@@ -0,0 +1,7 @@
+#Written by Shiven Desai
+#This program asks the user to input their name age, etc.
+#Asks the user to enter their full name and age
+full_name = (input('Enter your full name: '))
+age = int(input('Enter your age: '))
+#prints full name and age in correct format
+print(full_name + ',', age)
