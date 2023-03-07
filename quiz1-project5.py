7fb0ae2867eed81dbfce9456654ca9a818db08b2
Shiven Desai <shiven.desai14@gmail.com>
2023-03-06 19:25:33 -0800

    Made slight changes

--- quiz1-project5.py
+++ quiz1-project5.py
@@ -1,7 +1,7 @@
 #Written by Shiven Desai
 #This program is an extension to project 4
 # prompt user to enter sales
-sales = float(input("Enter sales: $"))
+sales = float(input('Enter sales: $'))
 
 # determine salary based on sales range
 if sales >= 50000 and sales <= 60000:
@@ -14,7 +14,7 @@ elif sales >= 90000 and sales <= 100000:
   salary = 90000
   commission_rate = 0.3
 else:
-  print("Invalid sales amount entered.")
+  print('Invalid sales amount entered.')
   exit()
 
 # calculate commission and total salary
@@ -26,4 +26,4 @@ formatted_sales = "${:,.2f}".format(sales)
 formatted_total_salary = "${:,.2f}".format(total_salary)
 
 # display result
-print(f"For sales of {formatted_sales}, the total salary including commission is {formatted_total_salary}.")
+print(f'For sales of {formatted_sales}, the total salary including commission is {formatted_total_salary}.')
